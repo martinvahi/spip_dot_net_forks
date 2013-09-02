8b5b34b474c2d07cd12efa840497a9924a6b5bca
Martin Vahi <martin.vahi@softf1.com>
2013-09-02 04:55:38 +0300

    first commit

---
+++ forked_from_v_3_0/ecrire/lang/public_et.php
@@ -0,0 +1,121 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/public?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accueil_site' => 'Avalehekülg',
+	'article' => 'Artikkel',
+	'articles' => 'Artikklid',
+	'articles_auteur' => 'Antud autori artiklid',
+	'articles_populaires' => 'Populaarseimad artiklid',
+	'articles_rubrique' => 'Siinse peatüki artiklid',
+	'aucun_article' => 'Siin artiklid puuduvad.',
+	'aucun_auteur' => 'Siin autorid puuduvad',
+	'aucun_site' => 'Siin lingid puuduvad',
+	'aucune_breve' => 'Siin uudised puuduvad',
+	'aucune_rubrique' => 'Siin peatükid puuduvad',
+	'auteur' => 'Autor',
+	'autres' => 'Teised',
+	'autres_breves' => 'Teised uudised',
+	'autres_groupes_mots_clefs' => 'Teised võtmesõnade grupid',
+	'autres_sites' => 'Teised kodulehed',
+
+	// B
+	'bonjour' => 'Tere',
+
+	// C
+	'commenter_site' => 'Comment on this site',
+	'contact' => 'Kontakt',
+	'copie_document_impossible' => 'Antud dokumenti ei ole võimalik kopeerida',
+
+	// D
+	'date' => 'Kuupäev',
+	'dernier_ajout' => 'Värskeimad uuendused',
+	'dernieres_breves' => 'Värskeimad uudised',
+	'derniers_articles' => 'Uusimad artiklid',
+	'derniers_commentaires' => 'Uusimad kommentaarid',
+	'derniers_messages_forum' => 'Uusimad foorumi-postitused',
+
+	// E
+	'edition_mode_texte' => 'Vaid teksti-režiim',
+	'en_reponse' => 'Replying to:',
+	'en_resume' => 'Kokkuvõte',
+	'envoyer_message' => 'Sõnumi saatmine',
+	'espace_prive' => 'Privaat-ruum',
+
+	// F
+	'formats_acceptes' => 'Aksepteeritavad formaadid: @formats@.',
+
+	// H
+	'hierarchie_site' => 'Sisu kaart:',
+
+	// J
+	'jours' => 'päevad',
+
+	// L
+	'lien_connecter' => 'Sisenemine',
+
+	// M
+	'meme_auteur' => 'Samalt autorilt',
+	'meme_rubrique' => 'Also in this section',
+	'memes_auteurs' => 'Samadelt autoritelt',
+	'message' => 'Teade',
+	'messages_forum' => 'Foorumi-postitused',
+	'messages_recents' => 'Uusimad foorumi-postitused',
+	'mots_clef' => 'Võtmesõna',
+	'mots_clefs' => 'Võtmesõnad',
+	'mots_clefs_meme_groupe' => 'Siinse grupi ülejäänud võtmesõnad',
+
+	// N
+	'navigation' => 'Sirvimine',
+	'nom' => 'Nimi',
+	'nouveautes' => 'What\'s new',
+	'nouveautes_web' => 'What\'s new on the Web',
+	'nouveaux_articles' => 'Uued artiklid',
+	'nouvelles_breves' => 'Latest news items',
+
+	// P
+	'page_precedente' => 'eelmine lehekülg',
+	'page_suivante' => 'järgmine lähekülb',
+	'par_auteur' => 'by ',
+	'participer_site' => 'You can take active part in this website and write your own articles by signing up here. You will receive an email with your account information for the private area of the site.',
+	'plan_site' => 'Sisu kaart',
+	'popularite' => 'Populaarsus',
+	'poster_message' => 'Teate postitamine',
+	'proposer_site' => 'You can suggest a website for inclusion in this section:',
+
+	// R
+	'repondre_article' => 'Comment on this article',
+	'repondre_breve' => 'Comment on this news item',
+	'resultats_recherche' => 'Otsitulemused',
+	'retour_debut_forums' => 'Tagasi foorumi algusse',
+	'rubrique' => 'Peatükk',
+	'rubriques' => 'Peatükid',
+
+	// S
+	'signatures_petition' => 'Signatuurid',
+	'site_realise_avec_spip' => 'Siinne lehekülg on loodud kasutades SPIP-nimelist avatud tarkvara.',
+	'sites_web' => 'Koduleheküljed',
+	'sous_rubriques' => 'Alampeatükid',
+	'spam' => 'Rämpspost ("spam")',
+	'suite' => 'jätkan',
+	'sur_web' => 'Interneti avarustes',
+	'syndiquer_rubrique' => 'Peatükki registreerumine',
+	'syndiquer_site' => 'Koduleheküljele registreerumine',
+
+	// T
+	'texte_lettre_information' => 'Siin on antud kodulehekülje uudiskiri',
+	'texte_lettre_information_2' => 'Antud kodulehekülg sisaldab uudiseid, mis publitseeritud alates',
+
+	// V
+	'ver_imprimer' => 'Trükitav versioon',
+	'voir_en_ligne' => 'Online-versioon',
+	'voir_squelette' => 'antud lehekülje malli näitamine'
+);
+
+?>
